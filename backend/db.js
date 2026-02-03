3689789039da1294450e46cfc8a1c37dcfcd1339
Ahamed311 <Seydouahamed9@gmail.com>
2026-02-03 17:14:37 +0100

    ajout

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 const { Pool } = require("pg");
-require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "courses",
-  port: process.env.DB_PORT || 5432,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
 module.exports = pool;
